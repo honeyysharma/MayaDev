6333e407320d8c68e9b3d45b808ad75b026727d2
honeyysharma <honey.y.sharma@gmail.com>
2017-03-24 17:06:23 +0530

    Defined methods for the asset list as per type.

--- AssetList.py
+++ AssetList.py
@@ -3,28 +3,27 @@ import maya.cmds as cmds
 def isGroup(node):
     children = cmds.listRelatives(node, children=1)
     for child in children:
-        if cmds.nodeType(child) == "transform":
-            return True
-        else:
+        if cmds.nodeType(child) != "transform":
             return False
+        return True 
 
-cmds.select(cmds.ls(assemblies=True))    
+def getAssetList(type):
+    topGroupNodes = cmds.ls(assemblies=True)
+    assetNodes = filter(lambda node: isGroup(node), allNodes)
+    filteredList = []
 
-topGroupNodes = cmds.ls(assemblies=True)
-assetNodes = filter(lambda node: isGroup(node), allNodes)
-envirList = []
-charList = []
-propList = []
-
-for node in assetNodes:
-    filename = cmds.referenceQuery(node, filename=True)
-    if "environment" in filename:
-        envirList.append(node)
-    elif "character" in filename:
-        charList.append(node)
-    else:
-        propList.append(node)
-        
-print envirList
-print charList
-print propList
+    for node in assetNodes:
+        if "shot_cam" not in node:
+            filename = cmds.referenceQuery(node, filename=True)
+            if type == "ENVIR" and "environment" in filename:
+                filteredList.append(node)
+            elif type == "CHAR" and "character" in filename:
+                filteredList.append(node)
+            elif type == "PROP" and "prop" in filename:
+                filteredList.append(node)
+                
+    return filteredList
+            
+print getAssetList("ENVIR")
+print getAssetList("CHAR")
+print getAssetList("PROP")

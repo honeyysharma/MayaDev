dd2026df0b70c7310c2150a6cdc30c2237bf593b
honeyysharma <honey.y.sharma@gmail.com>
2017-04-11 16:53:46 +0530

    Added code to populate assets.

--- RenderLayerController.py
+++ RenderLayerController.py
@@ -4,50 +4,59 @@ from RenderLayers import EnvirLayer, CharLayer, importRenderSetup, exportRenderS
 
 class RenderLayerController(object):
     def __init__(self):
-		pass
-    
-    def populateAssetTypes(self):
-		getAssets()
+		self.populateAssets = False
         
     def createEnvirLayer(self, layerName):
-        layer = EnvirLayer(layerName)
-        layer.createCollectionForAllLights()
-        #layer.turnOffCharLights()
-        layer.turnOffAllChar(False)
-        layer.createAllEnvirCollection()
-        #layer.switchToLayer()
+		if self.populateAssets == False:
+			getAssets()
+			self.populateAssets = True
+		layer = EnvirLayer(layerName)
+		layer.createCollectionForAllLights()
+		#layer.turnOffCharLights()
+		layer.turnOffAllChar(False)
+		layer.createAllEnvirCollection()
+		#layer.switchToLayer()
     
     def createCharLayer(self, layerName):
-        layer = CharLayer(layerName)
-        layer.createCollectionForAllLights()
-        #layer.turnOffEnvirLights()
-        layer.turnOffAllEnvir(True)
-        layer.createAllCharCollection()
-        #layer.switchToLayer()
+		if self.populateAssets == False:
+			getAssets()
+			self.populateAssets = True
+		layer = CharLayer(layerName)
+		layer.createCollectionForAllLights()
+		#layer.turnOffEnvirLights()
+		layer.turnOffAllEnvir(True)
+		layer.createAllCharCollection()
+		#layer.switchToLayer()
         
     def createCustomEnvirLayer(self, layerName, isCutoutChecked):
-        layer = EnvirLayer(layerName)
-        layer.createCollectionForAllLights()
-        #layer.turnOffCharLights()
-        
-        #toggle only Envir cutout
-        layer.turnOffAllEnvir(isCutoutChecked)
-        
-        layer.createCustomEnvirCollection(isCutoutChecked)
-        layer.turnOffAllChar(False)
-        #layer.switchToLayer()
+		if self.populateAssets == False:
+			getAssets()
+			self.populateAssets = True
+		layer = EnvirLayer(layerName)
+		layer.createCollectionForAllLights()
+		#layer.turnOffCharLights()
+
+		#toggle only Envir cutout
+		layer.turnOffAllEnvir(isCutoutChecked)
+
+		layer.createCustomEnvirCollection(isCutoutChecked)
+		layer.turnOffAllChar(False)
+		#layer.switchToLayer()
         
     def createCustomCharLayer(self, layerName, isCutoutChecked):
-        layer = CharLayer(layerName)
-        layer.createCollectionForAllLights()
-        #layer.turnOffEnvirLights()
-        layer.turnOffAllEnvir(True)
-        
-        #toggle only Char cutout
-        layer.turnOffAllChar(isCutoutChecked)
-        
-        layer.createCustomCharCollection(isCutoutChecked)
-        #layer.switchToLayer()
+		if self.populateAssets == False:
+			getAssets()
+			self.populateAssets = True
+		layer = CharLayer(layerName)
+		layer.createCollectionForAllLights()
+		#layer.turnOffEnvirLights()
+		layer.turnOffAllEnvir(True)
+
+		#toggle only Char cutout
+		layer.turnOffAllChar(isCutoutChecked)
+
+		layer.createCustomCharCollection(isCutoutChecked)
+		#layer.switchToLayer()
         
     def importSetup(self, filePath):
         importRenderSetup(filePath)
